df3acdcb983cbf99eac2ea41d8131545d51c5f7d
dzowera <idzowera@byupathway.edu>
2025-11-20 07:53:38 +0200

    added basic and robust error handling

--- server.js
+++ server.js
@@ -13,13 +13,12 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 
 const inventoryRoute = require("./routes/inventoryRoute") // <-- new inventory route
-
+const utilities = require("./utilities/") // <-- make sure utilities is in scope
 
 
 /* ***********************
  * View Engine and Templates
  *************************/
-
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
@@ -30,14 +29,31 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
-// Index route
-// app.get("/", function(req, res){
-//   res.render("index", {title: "Home"})
-// })
-
 // Index route (MVC style)
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
